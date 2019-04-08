188bc8b37ba8185218c02cc4640fac69c354ddc0
elisamartin <elisamv93@gmail.com>
2019-04-08 14:28:00 +0200

    Repair method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,35 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
+// a success(item) method that accepts an item object and returns a new item object modified according to the rules
+//defined by the client for enhancement success.
 function succeed(item) {
-  return { ...item };
+	return { ...item };
 }
 
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules
+//defined by the client for enhancement failure.
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
+// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100.
+//This method is the simplest of the three and would be a good starting point on this project.
 function repair(item) {
-  return { ...item };
+	if (typeof item.name !== 'string' || typeof item.durability !== 'number' || typeof item.enhancement !== 'number') {
+		throw new Error('Information is not correct');
+	}
+
+	let newItem = item;
+	newItem.durability = 100;
+	return newItem;
 }
 
+// a get() method for use when working on the stretch problem.
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,36 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('repair function', () => {
+	it('Should restores durability to 100', () => {
+		expect(
+			enhancer.repair({
+				name: 'Iron Sword',
+				durability: 50,
+				enhancement: 20
+			})
+		).toEqual({
+			name: 'Iron Sword',
+			durability: 100,
+			enhancement: 20
+		});
+	});
+
+	it('Error if information is not correct', () => {
+		expect(() =>
+			enhancer.repair({
+				name: 'Iron Sword',
+				durability: 'one hundred',
+				enhancement: 20
+			})
+		).toThrow();
+
+		expect(() =>
+			enhancer.repair({
+				name: 2,
+				durability: 100,
+				enhancement: 20
+			})
+		).toThrow();
+	});
+});
